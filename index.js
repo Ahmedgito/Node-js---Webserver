ea1f44b93ed1d4ffba67c6839804d444ee027419
Ahmedgito <mamooabid1@gmail.com>
2024-07-27 17:41:28 +0500

    added middle ware and auth

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-const http = require('http') ;
-const fs = require('fs') ; 
+const fs = require('fs');
+const express = require('express') ; //add Express 
+const { type } = require('os');
 
-const index = fs.readFileSync('index.html' ,'utf-8') ; 
-const data = JSON.parse(fs.readFileSync('data.json' ,'utf-8')) ; 
-const product = data.products ; 
+const index = fs.readFileSync('new.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products ; 
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    
-    switch(req.url){
-    case '/' :
-        res.setHeader('Content-Type', 'text/html');
-        res.end(index) ; 
-        break;
-    case '/api' : 
-        res.setHeader('Content-Type', 'application/json') ;
-        res.end(JSON.stringify(data)) ;
-        break ; 
-    case '/product' : 
-        res.setHeader('Content-Type', 'text/html') ;
-        let modified= index.replace('**title**' , product.title).replace('**description**',product.description)
-        res.end(modified) ;
-        break ; 
-    default:
-        res.writeHead(404);
-        res.end() ;         
+//making server in express
+const server = express() ;
 
-    }   
+//Using Middlewares
+server.use((req,res,next)=>{
+    console.log(req.method , req.ip , req.hostname)
+    next()   // we use next function so middleware can move to the next endpoint
 })
 
-server.listen(8080) ;  
+const Auth = ((req,res,next) =>{
+    console.log() ;
+    if (req.query.password) {
+    } else {
+        res.sendStatus(401)
+    }
+    next() ; 
+})
+
+server.use(Auth);
+
+// API - EndPoint - Route 
+server.get('/' , (req , res) =>{ 
+    res.json({type:'GET'})
+})
+server.post('/' , (req , res) =>{
+    res.json({type:'POST'})
+})
+server.delete('/' , (req , res) =>{
+    res.json({type:'DELETE'})
+})
+server.put('/' , (req , res) =>{
+    res.json({type:'PUT'})
+})
 
 
+
+
+server.get('/demo' , (req , res)=>{
+//    res.sendStatus(404)  // can be used to send status as a response  res.sendStatus(404).send('<h1>Custom text resposne</h1>') ;
+    res.json(products) // used to send json file in a response 
+//    res.send(''<h1>hello</h1>) can be used to send a custom response 
+//    res.sendFile('D:/NODE JS & MONGO DB/Node js/nodejs/new.html') ; used to send a file in response
+})
+
+//starts server
+server.listen(8080 , ()=>{
+    console.log('Server Started on Port 8080')
+}) ; 
