f41ee2f44333fa0eb054f212d177a976c7de5dfb
Ahmedgito <mamooabid1@gmail.com>
2024-07-31 16:29:23 +0500

    added REST Crud Api operations of GET and POST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express') ; //add Express 
 const { type } = require('os');
+const morgan = require('morgan');
 
 const index = fs.readFileSync('new.html', 'utf-8');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
@@ -9,39 +10,47 @@ const products = data.products ;
 //making server in express
 const server = express() ;
 
+// Body Parser
+server.use(morgan('default'))
+server.use(express.json());
+ server.use(express.static('public')) ; 
+
 //Using Middlewares
-server.use((req,res,next)=>{
-    console.log(req.method , req.ip , req.hostname)
-    next()   // we use next function so middleware can move to the next endpoint
-})
+    server.use((req,res,next)=>{
+        console.log(req.method , req.ip , req.hostname)
+        next()   // we use next function so middleware can move to the next endpoint
+    })
 
-const Auth = ((req,res,next) =>{
-    console.log() ;
-    if (req.query.password) {
-    } else {
-        res.sendStatus(401)
-    }
-    next() ; 
-})
-
-server.use(Auth);
 
 // API - EndPoint - Route 
-server.get('/' , (req , res) =>{ 
-    res.json({type:'GET'})
+
+// Read GET /products 
+server.get('/products' , (req , res) =>{ 
+    res.json(products) ; 
 })
-server.post('/' , (req , res) =>{
-    res.json({type:'POST'})
+
+//Read GET /product
+server.get('/products/:id' , (req , res) =>{ 
+    const id  = +req.params.id ; 
+    const product = products.find(p=>p.id===id)
+    res.json(product) ; 
 })
+
+// Create post Products
+server.post('/products' , (req , res) =>{
+    console.log(req.body) //rem to use body parsers.
+    products.push(req.body)
+    res.json(req.body) ; 
+})
+
+
+
 server.delete('/' , (req , res) =>{
     res.json({type:'DELETE'})
 })
 server.put('/' , (req , res) =>{
     res.json({type:'PUT'})
 })
-
-
-
 
 server.get('/demo' , (req , res)=>{
 //    res.sendStatus(404)  // can be used to send status as a response  res.sendStatus(404).send('<h1>Custom text resposne</h1>') ;
